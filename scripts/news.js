412b4d62d77417f76e7f0cbef6cbae6ef69a0fa8
Ben OReillygit config --global user.email Ben <neb.oreilly@gmail.com>
2020-06-19 20:41:10 -0400

    cors

--- scripts/news.js
+++ scripts/news.js
@@ -35,4 +35,8 @@ var newsSettings = {
 
 
 
-
+jQuery.ajaxPrefilter(function(options) {
+	if (options.crossDomain && jQuery.support.cors) {
+		options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+	}
+});
