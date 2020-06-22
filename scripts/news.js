d881e64f171db580477de2a2882c38e17f868f2b
Ben OReillygit config --global user.email Ben <neb.oreilly@gmail.com>
2020-06-21 21:20:17 -0400

    updates

--- scripts/news.js
+++ scripts/news.js
@@ -2,13 +2,13 @@
 var newsSettings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://newsapi.org/v2/top-headlines?country=us&apiKey=bd75295462f64e8c93b7d33b9c9895fd",
-	// "url": "https://newsapi.org/v2/top-headlines?sources=google-news&apiKey=bd75295462f64e8c93b7d33b9c9895fd",
-	// "url": "https://newsapi.org/v2/everything?domains=techcrunch.com,news.google.com,politico.com,nextbigfuture.com,reddit.com/r/all,wsj.com&country=us&apiKey=bd75295462f64e8c93b7d33b9c9895fd",
+	// "url": "https://newsapi.org/v2/top-headlines?country=us&apiKey=bd75295462f64e8c93b7d33b9c9895fd",
+	"url": "https://newsapi.org/v2/top-headlines?sources=google-news,politico,npr,techcrunch&apiKey=bd75295462f64e8c93b7d33b9c9895fd",
+	// "url": "https://newsapi.org/v2/everything?domains=techcrunch.com,news.google.com,politico.com,npr.com&apiKey=bd75295462f64e8c93b7d33b9c9895fd",
 	"method": "Get",
 	"headers": {"Access-Control-Allow-Origin": "*", "X-Requested-With":"XMLHttpRequest"},
 	"data": "{}"
-  }
+}
 
 
   
@@ -26,6 +26,7 @@ var newsSettings = {
 				<div class="news-text-content">
 					<h5 class="news-title"><a href="${article.url}" target="_blank">${article.title}</a></h5>
 					<h6 class="news-descrip">${article.description}</h6>
+					<h6 class="news-timestamp">${article.publishedAt}</h6>
 				</div> 
 			  </div>`
 			)
@@ -35,6 +36,14 @@ var newsSettings = {
 		</div>
 		</div>
 	  `;
+	  var getNewsTime = document.querySelectorAll('.news-timestamp');
+	  var timeArr = Array.from(getNewsTime);
+	  timeArr.forEach(function(item){
+		var publishTime = item.innerText;
+		var newsTimeStamp = new Date(publishTime);	
+		var newsTimeStampConvert = newsTimeStamp.toLocaleDateString('en-US');
+		item.innerText = newsTimeStampConvert;
+	  });
 });
 
 
