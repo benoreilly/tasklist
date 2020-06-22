b3ae0bfda9c1480287d81242be65605816ebe67a
Ben OReillygit config --global user.email Ben <neb.oreilly@gmail.com>
2020-06-21 22:34:15 -0400

    misc

--- scripts/news.js
+++ scripts/news.js
@@ -6,7 +6,7 @@ var newsSettings = {
 	"url": "https://newsapi.org/v2/top-headlines?sources=google-news,politico,npr,techcrunch&apiKey=bd75295462f64e8c93b7d33b9c9895fd",
 	// "url": "https://newsapi.org/v2/everything?domains=techcrunch.com,news.google.com,politico.com,npr.com&apiKey=bd75295462f64e8c93b7d33b9c9895fd",
 	"method": "Get",
-	"headers": {"Access-Control-Allow-Origin": "*", "X-Requested-With":"XMLHttpRequest"},
+	"headers": {'X-Requested-With': 'XMLHttpRequest'},
 	"data": "{}"
 }
 
