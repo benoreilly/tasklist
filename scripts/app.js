b690d433dbe6d819790feadd09c0c43ed52dc141
Ben OReillygit config --global user.email Ben <neb.oreilly@gmail.com>
2020-06-29 19:20:59 -0400

    music updates

--- scripts/app.js
+++ scripts/app.js
@@ -6,11 +6,124 @@ const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 const modalTrigger = document.querySelector('.modal-trigger');
-// const checked = $('#priCheck').is(':checked');
-//const sortNameBtn = document.getElementById('sortBtn');
-// const draggable_list = document.getElementById('draggable-list')
+
+const musicContainer = document.getElementById('music_container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+const audio = document.getElementById('audio');
+const progress = document.getElementById('music-progress');
+const progressContainer = document.getElementById('progress_container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
 
 
+// Song Titles
+
+const songs = ['Orange'];
+
+// Keep track of song
+let songIndex = 0;
+
+//Initially load song details into DOM
+
+loadSong(songs[songIndex]);
+
+//Update song details
+function loadSong(song) {
+    title.innerText = song;
+    audio.src = `music/${song}.mp3`;
+    cover.src = `img/${song}.jpg`;
+}
+
+//Play song
+
+function playSong() {
+    musicContainer.classList.add('play');
+    playBtn.querySelector('i.small.material-icons').innerText = "pause_circle_outline";
+
+    audio.play();
+}
+
+//Pause song
+
+function pauseSong() {
+    musicContainer.classList.remove('play');
+    playBtn.querySelector('i.small.material-icons').innerText = "play_circle_outline";
+
+    audio.pause();
+}
+
+// Previous Song
+
+function prevSong() {
+    songIndex--;
+
+    if(songIndex < 0){
+        songIndex = songs.length - 1;
+    }
+
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
+// Next Song
+function nextSong() {
+    songIndex++;
+
+    if(songIndex > songs.length - 1){
+        songIndex = 0;
+    }
+
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
+// Update progress bar
+
+function updateProgress(e) {
+    const {duration, currentTime} = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+}
+
+// Set progress bar
+
+function setProgress(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+
+    audio.currentTime = (clickX / width) * duration;
+}
+
+// Event listener for music
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play');
+
+    if(isPlaying) {
+        pauseSong();
+    } else {
+        playSong();
+    }
+});
+
+// Change song
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+// Time/song update
+
+audio.addEventListener('timeupdate', updateProgress);
+
+//Click on progress bar
+progressContainer.addEventListener('click', setProgress);
+
+//Song ends
+audio.addEventListener('ended', nextSong);
 
 // Get date
 
