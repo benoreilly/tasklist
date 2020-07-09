59925c50e22df160c657ab90efbc2da12cc0883f
Ben OReillygit config --global user.email Ben <neb.oreilly@gmail.com>
2020-07-09 12:36:51 -0400

    update

--- scripts/app.js
+++ scripts/app.js
@@ -391,11 +391,12 @@ function removeTaskFromLocalStorage(taskItem) {
     tasks.forEach(function (task, index) {
         
         var dateSplitter = task.split('taskSubmitDate=');
-        var taskAddDate = taskItem.firstChild.innerText.trim() + " " + 'taskSubmitDate=' + dateSplitter[1];
+        var taskAddDate;
+        taskAddDate = taskItem.firstChild.innerText == undefined ? '' : taskItem.firstChild.innerText.trim() + " " + 'taskSubmitDate=' + dateSplitter[1];
         
-        var taskPriCheck = taskItem.firstChild.innerText.trim() + "!" + " " + 'taskSubmitDate=' + dateSplitter[1];
+        var taskPriCheck = taskItem.firstChild.innerText == undefined ? '' : taskItem.firstChild.innerText.trim() + "!" + " " + 'taskSubmitDate=' + dateSplitter[1];
        
-        var taskWisCheck = taskItem.firstChild.innerText.trim() + "#" + " " + 'taskSubmitDate=' + dateSplitter[1];
+        var taskWisCheck = taskItem.firstChild.innerText == undefined ? '' : taskItem.firstChild.innerText.trim() + "#" + " " + 'taskSubmitDate=' + dateSplitter[1];
       
         if (taskAddDate === task) {
             tasks.splice(index, 1);
